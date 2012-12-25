2b1b622d6c85c63cfde1f25164673c25ff7fc7ad
Andrew Hart <ahart@211dev.(none)>
2012-12-25 07:39:40 -0800

    add configuration scripts for cube, start/stop scripts for dash

---
+++ etc/cube/evaluator-config.js
@@ -0,0 +1,9 @@
+// Default configuration for development.
+module.exports = {
+  "mongo-host": "127.0.0.1",
+  "mongo-port": 27018,
+  "mongo-database": "dash",
+  "mongo-username": null,
+  "mongo-password": null,
+  "http-port": 1081
+};
